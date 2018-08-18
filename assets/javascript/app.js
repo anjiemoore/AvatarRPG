0773faa17783abfaf8947d54dc15a419d2564bdb
Anjie <anjelinamoore@gmail.com>
2018-08-17 20:09:01 -0600

    characters added

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+const characters = {
+    'Aang': {
+        name: 'Aang',
+        health: 200,
+        attack: 10,
+        image: 'assets/images/aang.png',
+        enemyAttack: 20
+    },
+    'Zuko': {
+        name: 'Zuko',
+        health: 175,
+        attack: 12,
+        image: 'assets/images/aang.png',
+        enemyAttack: 18
+    },
+    'Katara': {
+        name: 'Katara',
+        health: 150,
+        attack: 8,
+        image: 'assets/images/aang.png',
+        enemyAttack: 9,
+    },
+    'Toph': {
+        name: 'Toph',
+        health: 120,
+        attack: 14,
+        image: 'assets/images/aang.png',
+        enemyAttack: 22
+    },
+    'Azula': {
+        name: 'Azula',
+        health: 200,
+        attack: 20,
+        image: 'assets/images/aang.png',
+        enemyAttack: 25
+    },
+    'Iroh': {
+        name: 'Iroh',
+        health: 180,
+        attack: 15,
+        image: 'assets/images/aang.png',
+        enemyAttack: 20
+    }
+};
