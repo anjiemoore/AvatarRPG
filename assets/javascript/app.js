c7d28647173f615a164aac0691cd1f80bbc71b1b
Anjie <anjelinamoore@gmail.com>
2018-08-17 20:24:00 -0600

    images added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,42 +3,42 @@ const characters = {
         name: 'Aang',
         health: 200,
         attack: 10,
-        image: 'assets/images/aang.png',
+        image: 'assets/images/Aang.png',
         enemyAttack: 20
     },
     'Zuko': {
         name: 'Zuko',
         health: 175,
         attack: 12,
-        image: 'assets/images/aang.png',
+        image: 'assets/images/Zuko.png',
         enemyAttack: 18
     },
     'Katara': {
         name: 'Katara',
         health: 150,
         attack: 8,
-        image: 'assets/images/aang.png',
+        image: 'assets/images/Katara.png',
         enemyAttack: 9,
     },
     'Toph': {
         name: 'Toph',
         health: 120,
         attack: 14,
-        image: 'assets/images/aang.png',
+        image: 'assets/images/Toph.png',
         enemyAttack: 22
     },
     'Azula': {
         name: 'Azula',
         health: 200,
         attack: 20,
-        image: 'assets/images/aang.png',
+        image: 'assets/images/Azula.png',
         enemyAttack: 25
     },
     'Iroh': {
         name: 'Iroh',
         health: 180,
         attack: 15,
-        image: 'assets/images/aang.png',
+        image: 'assets/images/Iroh.png',
         enemyAttack: 20
     }
 };
